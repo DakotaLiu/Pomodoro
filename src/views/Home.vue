bed7053e0f14f2f414b39478c550898bdddeb77e
Dakota Liu <fangyu.liu.print@gmail.com>
2020-06-01 09:57:43 +0800

    fixed format

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,87 +16,87 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       // 0 = 停止
       // 1 = 播放
       // 2 = 暫停
       status: 0,
       timer: 0
-    };
+    }
   },
   computed: {
-    currentText() {
+    currentText () {
       return this.current.length > 0
         ? this.current
         : this.todos.length > 0
-        ? "點擊開始"
-        : "沒有事項";
+          ? '點擊開始'
+          : '沒有事項'
     },
-    timetext() {
-      const m = Math.floor(this.timeleft / 60);
-      const s = Math.floor(this.timeleft % 60);
-      return `${m} : ${s}`;
+    timetext () {
+      const m = Math.floor(this.timeleft / 60)
+      const s = Math.floor(this.timeleft % 60)
+      return `${m} : ${s}`
     },
-    alarm() {
-      return this.$store.getters.alarm;
+    alarm () {
+      return this.$store.getters.alarm
     },
-    timeleft() {
-      return this.$store.getters.timeleft;
+    timeleft () {
+      return this.$store.getters.timeleft
     },
-    current() {
-      return this.$store.getters.current;
+    current () {
+      return this.$store.getters.current
     },
-    todos() {
-      return this.$store.getters.todos;
+    todos () {
+      return this.$store.getters.todos
     }
   },
   methods: {
-    start() {
+    start () {
       if (this.status === 2) {
         // 暫停後繼續
-        this.status = 1;
+        this.status = 1
         this.timer = setInterval(() => {
-          this.$store.commit("countdown");
+          this.$store.commit('countdown')
           if (this.timeleft <= 0) {
-            this.finish(false);
+            this.finish(false)
           }
-        }, 1000);
+        }, 1000)
       } else {
         // 開始新倒數
         if (this.todos.length > 0) {
-          this.$store.commit("start");
-          this.status = 1;
+          this.$store.commit('start')
+          this.status = 1
           this.timer = setInterval(() => {
-            this.$store.commit("countdown");
+            this.$store.commit('countdown')
             if (this.timeleft <= 0) {
-              this.finish(false);
+              this.finish(false)
             }
-          }, 1000);
+          }, 1000)
         }
       }
     },
-    finish(skip) {
-      clearInterval(this.timer);
-      this.status = 0;
-      this.$store.commit("finish");
+    finish (skip) {
+      clearInterval(this.timer)
+      this.status = 0
+      this.$store.commit('finish')
 
       if (!skip) {
-        const audio = new Audio();
-        audio.src = "./alarms/" + this.alarm;
-        audio.play();
+        const audio = new Audio()
+        audio.src = './alarms/' + this.alarm
+        audio.play()
       }
 
       if (this.todos.length > 0) {
-        this.start();
+        this.start()
       } else {
-        alert("結束");
+        alert('結束')
       }
     },
-    pause() {
-      clearInterval(this.timer);
-      this.status = 2;
+    pause () {
+      clearInterval(this.timer)
+      this.status = 2
     }
   }
-};
+}
 </script>
